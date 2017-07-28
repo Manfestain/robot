bf5f20bd33136f114b70c50a352e37d76634cb1a
Beans Lee <liguang345@126.com>
2017-07-28 20:37:23 +0800

    wechatRobot Program with note

--- WeChatRobot/mywechatrobot.py
+++ WeChatRobot/mywechatrobot.py
@@ -4,9 +4,9 @@
 import itchat
 from tuling import getResponse
 
-@itchat.msg_register(itchat.content.TEXT)
+@itchat.msg_register(itchat.content.TEXT)   # 监听
 def text_reply(msg):
-    return getResponse(msg['Text']).get('text')
+    return getResponse(msg['Text']).get('text')   # 返回为json,利用get()方法拿到机器人的回复
 
-itchat.auto_login()
+itchat.auto_login()  # 登陆
 itchat.run()
--- WeChatRobot/tuling.py
+++ WeChatRobot/tuling.py
@@ -2,13 +2,14 @@
 
 import requests
 
+# 发送请求
 def getResponse(_info):
-    message = _info.encode('utf-8')
-    apiUrl = 'http://www.tuling123.com/openapi/api'
+    message = _info.encode('utf-8')  # 查看官方文档得知，必须以utf-8进行编码
+    apiUrl = 'http://www.tuling123.com/openapi/api'  # 机器人请求的url
     data = {
-        'key': '21899823d93449ffa329eca31bba8f7e',
+        'key': '21899823d93449ffa329eca31bba8f7e',   # 机器人的APIKEY
         'info': message,
         'useid': 'wochat-robot'
     }
-    r = requests.post(apiUrl, data=data).json()
+    r = requests.post(apiUrl, data=data).json()  # 使用requests请求
     return r
